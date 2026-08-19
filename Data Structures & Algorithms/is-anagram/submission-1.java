8a44193919e2f58b5ee427d38659cc274f52eb91
Ishita choudhary <ishitaitgc@gmail.com>
2026-08-19 18:49:17 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+
+        int[] a=new int[26];
+        for(char ch: s.toCharArray()){
+            a[ch-'a']++;
+        }
+        for(char ch:t.toCharArray()){
+            a[ch-'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(a[i]!=0) return false;
+        }
+        return true;
+    }
+}

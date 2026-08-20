f8bee35ddb584d567749c3cc4593b97f8a21da06
Ishita choudhary <ishitaitgc@gmail.com>
2026-08-20 13:59:37 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(char ch: s.toCharArray()){
+            if(Character.isLetterOrDigit(ch)) sb.append(Character.toLowerCase(ch));
+        }
+        String s1=sb.toString();
+        String s2= new StringBuilder(s1).reverse().toString();
+        if(s1.equals(s2)) return true;
+        else return false;
+
+    }
+}
